4c0224f63540f4596a2213e1606d7eb0c83e7436
Daniel Rasmussen <development@dlras.net>
2015-06-14 17:49:03 -0500

    Added extension ForIEnumerable.Shuffle.

---
+++ Library/Extensions/ForIEnumerable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BitFn.Core.Extensions
+{
+	/// <summary>
+	///     Extension methods for the <see cref="IEnumerable{T}" /> interface.
+	/// </summary>
+	public static class ForIEnumerable
+	{
+		/// <summary>
+		///     Orders the enumerable in a random order.
+		/// </summary>
+		/// <param name="source">The source to use.</param>
+		/// <returns>The enumerable in a random order.</returns>
+		public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+		{
+			var rng = ThreadSafeRandom.Local;
+			return source.Shuffle(rng);
+		}
+
+		/// <summary>
+		///     Orders the enumerable in a random order, using the provided random number generator.
+		/// </summary>
+		/// <param name="source">The source to use.</param>
+		/// <param name="rng">The random number generator to use.</param>
+		/// <returns>The enumerable in a random order.</returns>
+		public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, Random rng)
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (rng == null) throw new ArgumentNullException(nameof(rng));
+
+			return ShuffleIterator(source, rng);
+		}
+
+		/// <remarks>http://stackoverflow.com/a/5807238/343238</remarks>
+		private static IEnumerable<T> ShuffleIterator<T>(IEnumerable<T> source, Random rng)
+		{
+			var buffer = source.ToList();
+			for (var i = 0; i < buffer.Count; i++)
+			{
+				var j = rng.Next(i, buffer.Count);
+				yield return buffer[j];
+
+				buffer[j] = buffer[i];
+			}
+		}
+	}
+}
---
+++ Tests/Extensions/ForIEnumerable/Shuffle.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+// ReSharper disable InvokeAsExtensionMethod
+
+namespace BitFn.Core.Tests.Extensions.ForIEnumerable
+{
+	[TestFixture]
+	public class Shuffle
+	{
+		[Test]
+		public void WhenGivenNullEnumerable_ShouldThrowArgumentNullExceptionBeforeEnumeration()
+		{
+			// Arrange
+			var enumerable = null as IEnumerable<object>;
+			var rng = new Random();
+
+			// Act
+			// ReSharper disable once ExpressionIsAlwaysNull
+			TestDelegate code = () => { Core.Extensions.ForIEnumerable.Shuffle(enumerable, rng); };
+
+			// Assert
+			Assert.Throws<ArgumentNullException>(code);
+		}
+
+		[Test]
+		public void WhenGivenNullRandom_ShouldThrowArgumentNullExceptionBeforeEnumeration()
+		{
+			// Arrange
+			var enumerable = Enumerable.Empty<object>();
+			var rng = null as Random;
+
+			// Act
+			// ReSharper disable once ExpressionIsAlwaysNull
+			TestDelegate code = () => { Core.Extensions.ForIEnumerable.Shuffle(enumerable, rng); };
+
+			// Assert
+			Assert.Throws<ArgumentNullException>(code);
+		}
+
+		[Test]
+		public void WhenGivenSingleItem_ShouldReturnEqual()
+		{
+			// Arrange
+			var enumerable = new[] {1};
+			var rng = new Random();
+
+			// Act
+			var actual = Core.Extensions.ForIEnumerable.Shuffle(enumerable, rng);
+
+			// Assert
+			CollectionAssert.AreEqual(enumerable, actual);
+		}
+
+		[Test]
+		public void WhenGivenMultipleItems_ShouldReturnEquivalent()
+		{
+			// Arrange
+			var enumerable = new[] {1, 2, 3};
+			var rng = new Random();
+
+			// Act
+			var actual = Core.Extensions.ForIEnumerable.Shuffle(enumerable, rng);
+
+			// Assert
+			CollectionAssert.AreEquivalent(enumerable, actual);
+		}
+
+		[Test]
+		public void WhenGivenNoRng_ShouldReturn()
+		{
+			// Arrange
+			var enumerable = new[] {1};
+
+			// Act
+			var actual = Core.Extensions.ForIEnumerable.Shuffle(enumerable);
+
+			// Assert
+			CollectionAssert.AreEqual(enumerable, actual);
+		}
+	}
+}
