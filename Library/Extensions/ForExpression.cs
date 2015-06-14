63f5b7fa78c800db555b68c5b1b55ca026740cf1
Daniel Rasmussen <development@dlras.net>
2015-06-14 16:01:56 -0500

    Added extension ForExpression.GetPropertyInfo.

---
+++ Library/Extensions/ForExpression.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace BitFn.Core.Extensions
+{
+	/// <summary>
+	///     Extension methods for the <see cref="Expression" /> class.
+	/// </summary>
+	public static class ForExpression
+	{
+		/// <summary>
+		///     Gets the property info for the property represented by the given lambda.
+		/// </summary>
+		/// <typeparam name="TSource">The source type.</typeparam>
+		/// <typeparam name="TProperty">The property type.</typeparam>
+		/// <param name="propertyLambda">The lambda representation of the property to get.</param>
+		/// <returns>The property info for the property represented by the given lambda.</returns>
+		/// <remarks>
+		///     Adapted from Cameron MacFarland's answer on StackOverflow.
+		///     http://stackoverflow.com/users/3820/
+		///     http://stackoverflow.com/a/672212/343238
+		/// </remarks>
+		public static PropertyInfo GetPropertyInfo<TSource, TProperty>(
+			this Expression<Func<TSource, TProperty>> propertyLambda)
+		{
+			if (propertyLambda == null) throw new ArgumentNullException(nameof(propertyLambda));
+
+			var member = propertyLambda.Body as MemberExpression;
+			if (member == null)
+			{
+				throw new ArgumentException($"Expression '{propertyLambda}' refers to a method, not a property.");
+			}
+			var propInfo = member.Member as PropertyInfo;
+			if (propInfo == null)
+			{
+				throw new ArgumentException($"Expression '{propertyLambda}' refers to a field, not a property.");
+			}
+			if (propInfo.ReflectedType == null ||
+			    (typeof (TSource) != propInfo.ReflectedType && !typeof (TSource).IsSubclassOf(propInfo.ReflectedType)))
+			{
+				throw new ArgumentException(
+					$"Expresion '{propertyLambda}' refers to a property that is not from type {typeof (TSource)}.");
+			}
+			return propInfo;
+		}
+	}
+}
---
+++ Tests/Extensions/ForExpression/GetPropertyInfo.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+using NUnit.Framework;
+
+// ReSharper disable InvokeAsExtensionMethod
+
+namespace BitFn.Core.Tests.Extensions.ForExpression
+{
+	[TestFixture]
+	public class GetPropertyInfo
+	{
+		[Test]
+		public void WhenGivenPublicPropertyLambda_ShouldMatch()
+		{
+			// Arrange
+			Expression<Func<TestPoco, object>> lambda = (_ => _.Property);
+
+			// Act
+			var actual = Core.Extensions.ForExpression.GetPropertyInfo(lambda);
+
+			// Assert
+			Assert.AreEqual(nameof(TestPoco.Property), actual.Name);
+		}
+		[Test]
+		public void WhenGivenNull_ShouldThrowArgumentNullException()
+		{
+			// Arrange
+			Expression<Func<TestPoco, object>> lambda = null;
+
+			// Act
+			// ReSharper disable once ExpressionIsAlwaysNull
+			TestDelegate code = () => { Core.Extensions.ForExpression.GetPropertyInfo(lambda); };
+
+			// Assert
+			Assert.Throws<ArgumentNullException>(code);
+		}
+
+		[Test]
+		public void WhenGivenPublicFieldLambda_ShouldThrowArgumentException()
+		{
+			// Arrange
+			Expression<Func<TestPoco, object>> lambda = (_ => _.Field);
+
+			// Act
+			TestDelegate code = () => { Core.Extensions.ForExpression.GetPropertyInfo(lambda); };
+
+			// Assert
+			Assert.Throws<ArgumentException>(code);
+		}
+
+		[Test]
+		public void WhenGivenPublicMethodLambda_ShouldThrowArgumentException()
+		{
+			// Arrange
+			Expression<Func<TestPoco, object>> lambda = (_ => _.Method());
+
+			// Act
+			TestDelegate code = () => { Core.Extensions.ForExpression.GetPropertyInfo(lambda); };
+
+			// Assert
+			Assert.Throws<ArgumentException>(code);
+		}
+
+		[Test]
+		public void WhenGivenPublicChildPropertyLambda_ShouldThrowArgumentException()
+		{
+			// Arrange
+			Expression<Func<TestPoco, object>> lambda = (_ => _.Child.Property);
+
+			// Act
+			TestDelegate code = () => { Core.Extensions.ForExpression.GetPropertyInfo(lambda); };
+
+			// Assert
+			Assert.Throws<ArgumentException>(code);
+		}
+
+		#region No Resharper
+
+		[ExcludeFromCodeCoverage]
+		private class TestPoco
+		{
+			public object Property { get; set; }
+#pragma warning disable 649
+			public object Field;
+#pragma warning restore 649
+			public ChildPoco Child { get; set; }
+
+			public object Method()
+			{
+				return new object();
+			}
+		}
+
+		[ExcludeFromCodeCoverage]
+		private class ChildPoco
+		{
+			public object Property { get; set; }
+		}
+
+		#endregion No Resharper
+	}
+}
