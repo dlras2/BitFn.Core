ea66d3a24f65fda8a5d33e3f9c4d0ea5023e871b
Daniel Rasmussen <development@dlras.net>
2015-06-14 17:29:44 -0500

    Added ThreadSafeRandom.

---
+++ Library/ThreadSafeRandom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace BitFn.Core
+{
+	/// <summary>
+	///     A class for getting a thread-static instance of <see cref="Random" />.
+	/// </summary>
+	/// <remarks>
+	///     Adapted from grenade's answer on StackOverflow.
+	///     http://stackoverflow.com/users/68115/
+	///     http://stackoverflow.com/a/1262619/343238
+	/// </remarks>
+	public static class ThreadSafeRandom
+	{
+		[ThreadStatic] private static Random _local;
+
+		/// <summary>
+		///     Gets a thread-static instance of <see cref="Random" /> seeded by tick and thread id.
+		/// </summary>
+		public static Random Local
+			=> _local ?? (_local = new Random(unchecked(Environment.TickCount*31 + Thread.CurrentThread.ManagedThreadId)));
+	}
+}
---
+++ Tests/ThreadSafeRandom/Local.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BitFn.Core.Tests.ThreadSafeRandom
+{
+	[TestFixture]
+	public class Local
+	{
+		[Test]
+		public void WhenGotten_ShouldReturnRandom()
+		{
+			// Arrange / Act
+			var actual = Core.ThreadSafeRandom.Local;
+
+			// Assert
+			Assert.IsInstanceOf<Random>(actual);
+		}
+	}
+}
