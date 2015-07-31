13727597c7ff0faedecb94327cf9080ccfb0a5ef
Daniel Rasmussen <development@dlras.net>
2015-07-31 09:46:27 -0500

    Added extensions ForIDictionary.GetOrAdd.

--- Library/Extensions/ForIDictionary.cs
+++ Library/Extensions/ForIDictionary.cs
@@ -10,6 +10,53 @@ namespace BitFn.Core.Extensions
 	public static class ForIDictionary
 	{
 		/// <summary>
+		///     Gets the element associated with the specified key, or adds an element with the provided key and value if none
+		///     exists.
+		/// </summary>
+		/// <param name="dictionary">The dictionary whose element to get or add.</param>
+		/// <param name="key">The object to use as the key of the element.</param>
+		/// <param name="newValue">The object to use as the value of the element to add if none exists.</param>
+		/// <returns>The existing element, or the element added.</returns>
+		public static TValue GetOrAdd<TKey, TValue>(
+			this IDictionary<TKey, TValue> dictionary, TKey key, TValue newValue)
+		{
+			Contract.Requires<ArgumentNullException>(dictionary != null);
+			Contract.Requires<ArgumentNullException>(key != null);
+
+			TValue result;
+			if (dictionary.TryGetValue(key, out result) == false)
+			{
+				result = newValue;
+				dictionary.Add(key, result);
+			}
+			return result;
+		}
+
+		/// <summary>
+		///     Gets the element associated with the specified key, or adds an element with the provided key and created value if
+		///     none exists.
+		/// </summary>
+		/// <param name="dictionary">The dictionary whose element to get or add.</param>
+		/// <param name="key">The object to use as the key of the element.</param>
+		/// <param name="newValueFactory">A factory to create the object to use as the value of the element to add if none exists.</param>
+		/// <returns>The existing element, or the element created and added.</returns>
+		public static TValue GetOrAdd<TKey, TValue>(
+			this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> newValueFactory)
+		{
+			Contract.Requires<ArgumentNullException>(dictionary != null);
+			Contract.Requires<ArgumentNullException>(key != null);
+			Contract.Requires<ArgumentNullException>(newValueFactory != null);
+
+			TValue result;
+			if (dictionary.TryGetValue(key, out result) == false)
+			{
+				result = newValueFactory();
+				dictionary.Add(key, result);
+			}
+			return result;
+		}
+
+		/// <summary>
 		///     Increments the element with the provided key by one. If no element exists, it is set to one instead.
 		/// </summary>
 		/// <param name="dictionary">The dictionary whose element to increment.</param>
---
+++ Tests/Extensions/ForIDictionary/GetOrAdd.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+
+// ReSharper disable InvokeAsExtensionMethod
+
+namespace BitFn.Core.Tests.Extensions.ForIDictionary
+{
+	[TestFixture]
+	public class GetOrAdd
+	{
+		[ExcludeFromCodeCoverage]
+		private static TestDelegate TestDelegate<TKey, TValue>(IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+		{
+			return () => Core.Extensions.ForIDictionary.GetOrAdd(dictionary, key, value);
+		}
+
+		[ExcludeFromCodeCoverage]
+		private static TestDelegate TestDelegate<TKey, TValue>(IDictionary<TKey, TValue> dictionary, TKey key,
+			Func<TValue> valueFactory)
+		{
+			return () => Core.Extensions.ForIDictionary.GetOrAdd(dictionary, key, valueFactory);
+		}
+
+		[ExcludeFromCodeCoverage]
+		private static object FactoryDelegate(object obj) => obj;
+
+		[Test]
+		public void WhenValueExists_ShouldGet()
+		{
+			// Arrange
+			var key = Guid.NewGuid().ToString();
+			var expected = new object();
+			var unexpected = new object();
+			var dictionary = new Dictionary<string, object> {[key] = expected};
+
+			// Act
+			var actual = Core.Extensions.ForIDictionary.GetOrAdd(dictionary, key, unexpected);
+
+			// Assert
+			Assert.AreSame(expected, actual);
+		}
+
+		[Test]
+		public void WhenValueMissing_ShouldGet()
+		{
+			// Arrange
+			var key = Guid.NewGuid().ToString();
+			var expected = new object();
+			var dictionary = new Dictionary<string, object>();
+
+			// Act
+			var actual = Core.Extensions.ForIDictionary.GetOrAdd(dictionary, key, expected);
+
+			// Assert
+			Assert.AreSame(expected, actual);
+		}
+
+		[Test]
+		public void WhenValueExistsWithFactory_ShouldGet()
+		{
+			// Arrange
+			var key = Guid.NewGuid().ToString();
+			var expected = new object();
+			var unexpected = new object();
+			var dictionary = new Dictionary<string, object> {[key] = expected};
+
+			// Act
+			var actual = Core.Extensions.ForIDictionary.GetOrAdd(dictionary, key, FactoryDelegate(unexpected));
+
+			// Assert
+			Assert.AreSame(expected, actual);
+		}
+
+		[Test]
+		public void WhenValueMissingWithFactory_ShouldGet()
+		{
+			// Arrange
+			var key = Guid.NewGuid().ToString();
+			var expected = new object();
+			Func<object> expectedFactory = () => expected;
+			var dictionary = new Dictionary<string, object>();
+
+			// Act
+			var actual = Core.Extensions.ForIDictionary.GetOrAdd(dictionary, key, expectedFactory);
+
+			// Assert
+			Assert.AreSame(expected, actual);
+		}
+
+		[Test]
+		public void WhenGivenNullDictionary_ShouldThrowArgumentNullException()
+		{
+			// Arrange
+			var dictionary = null as Dictionary<object, object>;
+			var key = new object();
+			var value = new object();
+
+			// Act
+			// ReSharper disable once ExpressionIsAlwaysNull
+			var code = TestDelegate(dictionary, key, value);
+
+			// Assert
+			Assert.Throws<ArgumentNullException>(code);
+		}
+
+		[Test]
+		public void WhenGivenNullKey_ShouldThrowArgumentNullException()
+		{
+			// Arrange
+			var dictionary = new Dictionary<object, object>();
+			var key = null as object;
+			var value = new object();
+
+			// Act
+			// ReSharper disable once ExpressionIsAlwaysNull
+			var code = TestDelegate(dictionary, key, value);
+
+			// Assert
+			Assert.Throws<ArgumentNullException>(code);
+		}
+
+		[Test]
+		public void WhenGivenNullDictionaryWithFactory_ShouldThrowArgumentNullException()
+		{
+			// Arrange
+			var dictionary = null as Dictionary<object, object>;
+			var key = new object();
+			var value = new object();
+
+			// Act
+			// ReSharper disable once ExpressionIsAlwaysNull
+			var code = TestDelegate(dictionary, key, FactoryDelegate(value));
+
+			// Assert
+			Assert.Throws<ArgumentNullException>(code);
+		}
+
+		[Test]
+		public void WhenGivenNullKeyWithFactory_ShouldThrowArgumentNullException()
+		{
+			// Arrange
+			var dictionary = new Dictionary<object, object>();
+			var key = null as object;
+			var value = new object();
+
+			// Act
+			// ReSharper disable once ExpressionIsAlwaysNull
+			var code = TestDelegate(dictionary, key, FactoryDelegate(value));
+
+			// Assert
+			Assert.Throws<ArgumentNullException>(code);
+		}
+
+		[Test]
+		public void WhenGivenNullFactory_ShouldThrowArgumentNullException()
+		{
+			// Arrange
+			var dictionary = new Dictionary<object, object>();
+			var key = new object();
+			var valueFactory = null as Func<object>;
+
+			// Act
+			// ReSharper disable once ExpressionIsAlwaysNull
+			var code = TestDelegate(dictionary, key, valueFactory);
+
+			// Assert
+			Assert.Throws<ArgumentNullException>(code);
+		}
+	}
+}
