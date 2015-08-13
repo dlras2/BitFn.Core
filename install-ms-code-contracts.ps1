efde4f4f199ac62e487cc01bb1d663917602cf32
Daniel Rasmussen <development@dlras.net>
2015-08-12 21:31:55 -0500

    Revert "Added code contract support via temp install."
    This reverts commit 97148bd347723fb67845524be149c90641e1fbf0.

--- install-ms-code-contracts.ps1
+++
@@ -1,4 +0,0 @@
-$msiPath = "$($env:USERPROFILE)\Contracts.devlab9ts.msi"
-(New-Object Net.WebClient).DownloadFile('https://visualstudiogallery.msdn.microsoft.com/1ec7db13-3363-46c9-851f-1ce455f66970/file/93972/17/Contracts.devlab9ts.msi', $msiPath)
-cmd /c start /wait msiexec /i $msiPath /quiet
-del $msiPath
